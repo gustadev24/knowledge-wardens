0cb3bb1e56b7182b8e5a4f1daaa9a9e3087ff896
JhonatanDczel <dev.dczel@gmail.com>
2025-04-01 23:56:30 -0500

    Updating particle rendering component

--- src/components/particles/MouseParticlesClient.tsx
+++ src/components/particles/MouseParticlesClient.tsx
@@ -2,19 +2,22 @@
 
 import dynamic from 'next/dynamic';
 
-const DynamicMouseParticles = dynamic(
-  () => import('react-mouse-particles'),
+const CustomMouseParticles = dynamic(
+  () => import('./CustomMouseParticles'),
   { ssr: false },
 );
 
 export default function MouseParticlesClient() {
   return (
-    <div
-      className="fixed top-0 left-0 w-screen h-screen pointer-events-none z-[9999]"
-    >
-      <DynamicMouseParticles
+    <div className="fixed top-0 left-0 w-screen h-screen pointer-events-none z-[9999]">
+      <CustomMouseParticles
         g={1}
-        color={['#37B8EF', '#2F9ECD', '#39BEF7', '#2783AB', '#1F6989', '#37B8EF', '#2F9ECD', '#39BEF7', '#2783AB', '#1F6989', '#FFFFFF', '#F7EF39']}
+        color={[
+          '#37B8EF', '#2F9ECD', '#39BEF7',
+          '#2783AB', '#1F6989', '#37B8EF',
+          '#2F9ECD', '#39BEF7', '#2783AB',
+          '#1F6989', '#FFFFFF', '#F7EF39'
+        ]}
       />
     </div>
   );
