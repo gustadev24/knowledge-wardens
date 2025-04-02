22e68430def0046346adc5792ba97d932f124ce6
gustadev24 <lgsc21211@gmail.com>
2025-04-02 11:19:25 -0500

    Add packages and fix mouse particles component

--- src/components/particles/CustomMouseParticles.tsx
+++ src/components/particles/CustomMouseParticles.tsx
@@ -1,70 +1,72 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react';
-import Proton from 'proton-engine';
+import { useEffect, useRef } from 'react';
+import Proton, {
+  Alpha, Color, Emitter, Life, Mass, Radius, RandomDrift, Rate, Scale, Span, Velocity, Gravity,
+  DomRenderer,
+} from 'proton-engine';
 import RAFManager from 'raf-manager';
 
 export default function CustomMouseParticles({
   num = 3,
   color = ['#37B8EF', '#2F9ECD', '#39BEF7', '#2783AB', '#1F6989', '#37B8EF', '#2F9ECD', '#39BEF7', '#2783AB', '#1F6989', '#FFFFFF', '#F7EF39'],
   radius,
-  cull,
   life,
   tha,
   alpha,
   v = 0.65,
   g,
+}: {
+  num?: number,
+  color?: string[],
+  radius?: number,
+  life?: number,
+  tha?: number[],
+  alpha?: number,
+  v?: number,
+  g?: number,
 }) {
-  const containerRef = useRef(null);
-  const protonRef = useRef(null);
-  const emitterRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const container = document.createElement('div');
-    container.style.position = 'fixed';
-    container.style.left = '0px';
-    container.style.top = '0px';
-    container.style.zIndex = '9999';
-    container.style.pointerEvents = 'none';
-    container.id = `mouse_ps_${(Math.random() * 999999) >> 0}`;
-    document.body.appendChild(container);
-    containerRef.current = container;
+    // sera como un portal?
+    // document.body.appendChild(container);
 
     const proton = new Proton();
-    protonRef.current = proton;
 
-    const emitter = new Proton.Emitter();
-    emitter.rate = new Proton.Rate(num);
+    const emitter = new Emitter();
+    emitter.rate = new Rate(num);
     emitter.damping = 0.008;
-    const particleLife = life ? new Proton.Life(life) : new Proton.Life(0.2, 0.5);
-    const particleAlpha = alpha || Proton.getSpan(0.25, 0.55);
-    const particleTha = tha ? new Proton.Span(tha[0], tha[1]) : new Proton.Span(0, 360);
-    let particleRadius = new Proton.Radius(2, 5);
+    const particleLife = life ? new Life(life) : new Life(0.2, 0.5);
+    const particleAlpha = alpha || new Span(0.25, 0.55);
+    const particleTha = tha ? new Span(tha[0], tha[1]) : new Span(0, 360);
+    let particleRadius = new Radius(2, 5);
     if (radius) {
-      particleRadius = new Proton.Radius(radius * 0.8, radius);
+      particleRadius = new Radius(radius * 0.8, radius);
     }
 
-    emitter.addInitialize(new Proton.Mass(1));
+    emitter.addInitialize(new Mass(1));
     emitter.addInitialize(particleRadius);
     emitter.addInitialize(particleLife);
-    emitter.addInitialize(new Proton.Velocity(new Proton.Span(v), particleTha, 'polar'));
+    emitter.addInitialize(new Velocity(new Span(v), particleTha, 'polar'));
 
-    emitter.addBehaviour(new Proton.Alpha(particleAlpha));
-    emitter.addBehaviour(new Proton.Color(color));
-    emitter.addBehaviour(new Proton.Scale(1, 0.1));
-    emitter.addBehaviour(new Proton.RandomDrift(10, 10, 0.2));
+    emitter.addBehaviour(new Alpha(particleAlpha));
+    emitter.addBehaviour(new Color(color));
+    emitter.addBehaviour(new Scale(1, 0.1));
+    emitter.addBehaviour(new RandomDrift(10, 10, 0.2));
     if (g) {
-      emitter.addBehaviour(new Proton.G(parseFloat(g)));
+      emitter.addBehaviour(new Gravity(g));
     }
 
     proton.addEmitter(emitter);
-    emitterRef.current = emitter;
 
-    const renderer = new Proton.DomRenderer(container);
-    proton.addRenderer(renderer);
+    if (containerRef.current) {
+      const renderer = new DomRenderer(containerRef.current);
+      proton.addRenderer(renderer);
+    }
 
     const ease = 0.7;
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: MouseEvent) => {
       const x = e.clientX;
       const y = e.clientY;
 
@@ -76,7 +78,7 @@ export default function CustomMouseParticles({
     document.body.addEventListener('mousemove', handleMouseMove, false);
 
     const renderLoop = () => {
-      proton && proton.update();
+      if (proton) proton.update();
     };
     RAFManager.add(renderLoop);
 
@@ -86,11 +88,15 @@ export default function CustomMouseParticles({
       if (proton) {
         proton.destroy();
       }
-      if (container) {
-        document.body.removeChild(container);
-      }
     };
-  }, [num, color, radius, life, tha, alpha, v, g]);
+  }, [num, color, radius, life, tha, alpha, v, g, containerRef]);
 
-  return null;
+  return (
+    <div
+      ref={containerRef}
+      // eslint-disable-next-line no-bitwise
+      id={`mouse_ps_${(Math.random() * 999999) >> 0}`}
+      className="fixed left-0 top-0 z-[9999] pointer-events-none"
+    />
+  );
 }
--- src/components/particles/MouseParticlesClient.tsx
+++ src/components/particles/MouseParticlesClient.tsx
@@ -2,6 +2,8 @@
 
 import dynamic from 'next/dynamic';
 
+// Es necesario la carga dinamica de este componente porque ProtonJS no es compatible con el renderizado del lado del servidor (SSR) de Next.js.
+
 const CustomMouseParticles = dynamic(
   () => import('./CustomMouseParticles'),
   { ssr: false },
@@ -16,7 +18,7 @@ export default function MouseParticlesClient() {
           '#37B8EF', '#2F9ECD', '#39BEF7',
           '#2783AB', '#1F6989', '#37B8EF',
           '#2F9ECD', '#39BEF7', '#2783AB',
-          '#1F6989', '#FFFFFF', '#F7EF39'
+          '#1F6989', '#FFFFFF', '#F7EF39',
         ]}
       />
     </div>
